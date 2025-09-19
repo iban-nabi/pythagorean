199281316ffae95ba878313f847ca0eaaaa5be05
Ivan Paguyo <ezrapaguyo09@gmail.com>
2025-09-19 21:04:21 +0800

    Update web config

--- src/main/java/com/phase_one/pythagorean/config/WebConfig.java
+++ src/main/java/com/phase_one/pythagorean/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET")
                         .allowCredentials(true);
             }
         };
